5667eddafac679593d36c5b42f20ee71680b54a8
Sturmannn <90060665+Sturmannn@users.noreply.github.com>
2021-09-17 09:03:15 +0300

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 4;
+	printf("a = %d\n", a);
+	char b = 'g';
+	printf("b = %c\n", b);
+	unsigned char c = 'f';
+	printf("c = %c\n", c);
+	short d = 23;
+	printf("d = %d\n", d);
+	unsigned short e = 2000;
+	printf("e = %d\n", e);
+	unsigned int f = 35025;
+	printf("f = %d\n", f);
+	long g = 1343215;
+	printf("g = %d\n", g);
+	unsigned long h = 764545645;
+	printf("h = %d\n", h);
+	return 0;
+}
